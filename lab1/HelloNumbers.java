0739c4364253e950bd57808611d655a6cd03ffb7
Chang <chang23333@gmail.com>
2018-07-02 14:19:08 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int total=0;
+        for(int x=0;x<10;x+=1){
+          total += x;
+          System.out.print(total + "  ");
+        }
+    }
+}
